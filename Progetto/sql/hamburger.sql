3bd3bd382fbe72193496faec73816ce3f3cb1cd3
Fabio De Palma <fabio.depalma@edu.unito.it>
2021-01-25 23:20:12 +0100

    db

--- Progetto/sql/hamburger.sql
+++ Progetto/sql/hamburger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 02, 2021 alle 19:45
+-- Creato il: Gen 25, 2021 alle 21:29
 -- Versione del server: 10.4.14-MariaDB
 -- Versione PHP: 7.4.11
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `hamburger`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `name_burger`
+--
+
+CREATE TABLE `name_burger` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `ingredients` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `name_burger`
+--
+
+INSERT INTO `name_burger` (`id`, `name`, `ingredients`) VALUES
+(7, 'carmine', 'tomato,bacon,meat,cheddar'),
+(8, 'Prova', 'salad,meat,pickle,bacon'),
+(12, 'antonio', 'salad, meat, pickle, ketchup'),
+(12, 'giuseppe', 'salad, grilled peppers, tomato, pickle sauce'),
+(12, 'mimmo', 'salad, onion, pineapple, egg');
 
 -- --------------------------------------------------------
 
@@ -42,7 +65,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `age`, `email`, `username`, `password`) VALUES
-(1, 'Mario', 'Rossi', '1987-12-10', 'maiorossi@gmail.com', 'mariorossi', 'bestrossi');
+(1, 'Mario', 'Rossi', '1987-12-10', 'maiorossi@gmail.com', 'mariorossi', '2bf65275cb7f5dc95febd7d46cd7d0af'),
+(2, 'Dante', 'Alighieri', '1265-05-22', 'dante@gmail.com', 'dante', '9077c3585038a90e34afe97a02bf4355'),
+(3, 'Otto', 'Kuasw', '1857-05-11', 'ottokuasw@gmail.com', 'ottokuasw', '7aa99f13e44dd25f7de294bc71abe4ff'),
+(5, 'damien', 'rice', '2222-02-22', 'damien@rice.com', 'daminerice', 'f3bdc5a3844813fb1d5e4b15fbfa6dbb'),
+(7, 'carmine', 'de palma', '1960-04-30', 'emy.car@virgilio.it', 'pippo', '712b7448ea3de70189aff5441d9e6ff4'),
+(8, 'Fabio', 'De Palma', '1995-11-26', 'fabio.depalma95@gmail.com', 'Frost', 'a4ef405a1fdc163412df52baa59368da'),
+(12, 'admin', 'admin', '1995-11-26', 'admin@admin.com', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(13, 'Antonio', 'Ricci', '1960-07-02', 'stricialanotizia@mediaset.it', 'antonioricci', 'ad7a76c41b183bdc2ecfc977e84d27d3');
 
 --
 -- Indici per le tabelle scaricate
@@ -62,7 +92,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
