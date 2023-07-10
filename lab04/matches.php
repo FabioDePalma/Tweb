27cf0e36c551a04c6acb93887ee555e00d8e0b7a
FabioDePalma <depalma.fabio95@gmail.com>
2023-07-10 02:30:18 +0200

    final

---
+++ lab04/matches.php
@@ -0,0 +1,12 @@
+<?php include "top.html"; ?>
+
+<form action="matches-submit.php" method = "GET">
+    <fieldset>
+        <legend>Returning User:</legend>
+        Name: <input type="text" name="name" id="name"><br>
+        <input type="submit" value="Submit">
+    </fieldset>
+    
+
+</form>
+<?php include "bottom.html"; ?>
